3b046575159e90789dbc58d59dab2b15e6d461f4
khademul <rkhademul8@gmail.com>
2021-11-25 19:44:11 +0600

    upto_product

---
+++ ecom/api/product/serializers.py
@@ -0,0 +1,11 @@
+
+from django.db import models
+from django.db.models import fields
+from rest_framework import serializers
+from .models import Product
+
+class ProductSerializer(serializers.HyperlinkedModelSerializer):
+    image=serializers.ImageField(max_length=None, allow_empty_file=False,allow_null=True,required=False)
+    class Meta:
+        model=Product
+        fields=('id','name','description','image','category')
